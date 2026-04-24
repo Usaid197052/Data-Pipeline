40f6489a9e23eda62b3987891ec8256511b0fadf
Usaid Ahmed <usaid33376@gmail.com>
2026-04-24 12:20:01 +0500

    Update ml_prep.py

--- DataPipeline/DataPipeline/script/ml_prep.py
+++ DataPipeline/DataPipeline/script/ml_prep.py
@@ -9,7 +9,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.dirname(BASE_DIR)
 
 data_path = os.path.join(PROJECT_ROOT, "data", "processed", "cleaned_data.csv")
-
 df = pd.read_csv(data_path)
 
 print("Data loaded.")
@@ -18,21 +17,13 @@ df['high_value_customer'] = df['total'] > 100
 X = df[['price', 'quantity']]
 y = df['high_value_customer']
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
+    X, y, test_size=0.2, random_state=42)
 
 print("Train/Test split completed.")
-
-# ---------------------------
-# PRINT RESULTS
-# ---------------------------
 print("Training samples:", len(X_train))
 print("Testing samples:", len(X_test))
-
 print("\nSample training data:")
 print(X_train.head())
-
 print("\nSample target values:")
 print(y_train.head())
-
 print("ML preparation complete.")
